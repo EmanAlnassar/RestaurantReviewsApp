020fcb0d9d2da99909029263c728dfb2210f9026
EmanAlnassar <eman.alnassar@gmail.com>
2019-11-19 16:15:13 +0300

    feat: Add map API

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiZW1hbmF0aW9uIiwiYSI6ImNrMnk1b2RpMzAyMjczZHQ4dXNvb2oxOW0ifQ.WetvbgyXqQa5FyuPXCzUpA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
