ebf6707fd4df72274177a49b8149684b68318d8e
EmanAlnassar <eman.alnassar@gmail.com>
2019-12-06 11:59:53 +0300

    feat: Install a service worker

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
+var CACHE_NAME = 'rest-site-cache-v1';
+var urlsToCache = [
+    '/',
+    '/img',
+    '/js',
+    '/css'
+];
 
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
