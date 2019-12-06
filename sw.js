86ae0c0ffa78fe8a722505577d043601b533029d
EmanAlnassar <eman.alnassar@gmail.com>
2019-12-06 12:02:16 +0300

    feat: Cache and update a service worker

--- sw.js
+++ sw.js
@@ -16,3 +16,33 @@ self.addEventListener('install', function (event) {
             })
     );
 });
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+
+    var cacheWhitelist = ['pages-cache-v1', 'rest-info-cache-v1'];
+
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
